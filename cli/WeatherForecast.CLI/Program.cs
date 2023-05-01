28e79da15eafcee32ac827c30563dae600390883
Dinindu Fernando <wdnfernando@gmail.com>
2023-05-02 02:27:58 +1000

    implement logging

--- cli/WeatherForecast.CLI/Program.cs
+++ cli/WeatherForecast.CLI/Program.cs
@@ -3,17 +3,34 @@ using WeatherForecast.Controllers;
 using WeatherForecast.Data;
 using WeatherForecast.Services;
 using WeatherForecast.Models;
+using Serilog;
+using Serilog.Core;
+
 namespace WeatherForecast.CLI
 {
     class Program
     {
+        private static readonly Logger _logger;
+
+        static Program()
+        {
+            //Set up logging
+            _logger = new LoggerConfiguration()
+           .MinimumLevel.Information()
+           .WriteTo.File("./logs/log.txt",
+               rollingInterval: RollingInterval.Day,
+               rollOnFileSizeLimit: true)
+           .CreateLogger();
+        }
+
         static void Main(string[] args)
         {
             // Parse the command line arguments
             CommandLine.Parser.Default.ParseArguments<Options>(args)
                 .WithParsed<Options>(o =>
                 {
-                    Console.WriteLine($"Getting Weather Forecast summary for {o.Days} day/s at Latitude: {o.Latitude} and Longitude: {o.Longitude}");
+                    string initialMessage = $"Getting Weather Forecast summary for {o.Days} day/s at Latitude: {o.Latitude} and Longitude: {o.Longitude}";
+                    _logger.Information(initialMessage);
 
                     var forecastRequest = new ForecastRequest()
                     {
@@ -24,14 +41,15 @@ namespace WeatherForecast.CLI
 
                     InitiateForecastSummary(forecastRequest).Wait();
                 });
+
+            _logger.Dispose();
         }
 
-        public static async Task<bool> InitiateForecastSummary(ForecastRequest forecastRequest)
+        static async Task<bool> InitiateForecastSummary(ForecastRequest forecastRequest)
         {
             bool success = false;
             string errorMessage = string.Empty;
 
-            //TODO: Get configurations from a common config file
             var fileName = Path.Combine("./output/", $"weatherExport_{DateTime.Today.ToString("yyyyMMdd")}.json");
 
             try
@@ -55,13 +73,16 @@ namespace WeatherForecast.CLI
             {
                 if (success)
                 {
-                    Console.WriteLine($"Successfully exported Weather Forecast to the file: {fileName}");
+                    string successMessage = $"Successfully exported Weather Forecast to the file: {fileName}";
+                    Console.WriteLine(successMessage);
+                    _logger.Information(successMessage);
                 }
                 else
                 {
                     Console.WriteLine("Failed to export Weather Forecast!");
                     Console.WriteLine($"Error(s):");
                     Console.WriteLine(errorMessage);
+                    _logger.Error(errorMessage);
                 }
             }
             return success;
